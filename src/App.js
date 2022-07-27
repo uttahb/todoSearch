1b2478a973c29f53a6c0b58159fb1236f707a2fb
Jishnu <jishnu.nb@neoito.com>
2022-07-27 12:50:36 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import Todosearch from './todoSearch'
+export default function App() {
+  return (
+    <div>
+      <Todosearch/>
+    </div>
+  )
+}
---
+++ src/todoSearch.js
@@ -0,0 +1,8 @@
+import React from 'react'
+// import env from 'env'
+export const Todosearch = () => {
+  return (
+   <p>Hello from todoSearch </p>
+  )
+}
+export default Todosearch
